046f25ae606e806edd0ccea66a55c3da469374cd
Brian Lee <brian@atlaslabs.ai>
2024-12-04 15:11:36 +0900

    revert back to code pipeline

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/YoursTruly
-git pull origin master
-npm install &&
-npm run build &&
-pm2 restart yours-truly
---
+++ scripts/start_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/yourapp
+pm2 start npm --name yours-truly -- run start
