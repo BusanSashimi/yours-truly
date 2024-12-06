e0813c584482c9adddce9c74751b93ab42f22ceb
Brian Lee <brian@atlaslabs.ai>
2024-12-06 15:39:38 +0900

    change code deploy scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-source ~/.nvm/nvm.sh
+export NVM_DIR="/home/ubuntu/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 echo "$PATH"
 cd /home/ubuntu/yours-truly
 npm install
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-source ~/.nvm/nvm.sh
+export NVM_DIR="/home/ubuntu/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 cd /home/ubuntu/yours-truly
 /home/ubuntu/.nvm/versions/node/v22.12.0/bin/pm2 start npm --name yours-truly -- run start
