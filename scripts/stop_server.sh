f591e9f6af33dffe1b928e27b8c8f5e5f778aff8
Brian Lee <brian@atlaslabs.ai>
2024-12-06 14:28:47 +0900

    add nvm configuration to script files

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-/home/ubuntu/.nvm/versions/node/v22.12.0/bin/pm2 delete yours-truly || true
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+pm2 delete yours-truly || true
