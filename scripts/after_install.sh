04cd6b7263d1c7a9765447350508ef5a1192c090
Brian Lee <brian@atlaslabs.ai>
2024-12-06 16:07:03 +0900

    after insrtall script fix

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,8 @@ export NVM_DIR="/home/ubuntu/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 echo "IS this working"
 echo "$PATH"
+echo npm -v
+echo node -v
 cd /home/ubuntu/yours-truly
-sudo npm install
-sudo npm run build
+npm install
+npm run build
