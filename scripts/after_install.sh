b77c2d2c33b93916e1a0ce2c04e1a163a5cf9f83
Brian Lee <brian@atlaslabs.ai>
2024-12-06 14:41:59 +0900

    fix shell script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source ~/.nvm/nvm.sh
+echo "$PATH"
 cd /home/ubuntu/yours-truly
-/home/ubuntu/.nvm/versions/node/v22.12.0/bin/npm install
-/home/ubuntu/.nvm/versions/node/v22.12.0/bin/npm run build
+npm install
+npm run build
