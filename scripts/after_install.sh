2e92e6939e4366d8d9d131ed321fce687583b0b4
Brian Lee <brian@atlaslabs.ai>
2024-12-06 15:43:06 +0900

    change script to sudo install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,5 +3,5 @@ export NVM_DIR="/home/ubuntu/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 echo "$PATH"
 cd /home/ubuntu/yours-truly
-npm install
-npm run build
+sudo npm install
+sudo npm run build
