35e1d541d2b0921009af135a011c8eae942d9183
Brian Lee <brian@atlaslabs.ai>
2024-12-07 11:05:43 +0900

    remove rm -rf

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,5 @@ export NVM_DIR="/home/ubuntu/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 echo "$PATH"
 cd /home/ubuntu/yours-truly
-rm -rf node_modules package-lock.json
 npm install
 npm run build
