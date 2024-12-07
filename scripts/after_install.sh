3f31bb3330ad35bfb255de13bfbf344e6b9845f3
Brian Lee <brian@atlaslabs.ai>
2024-12-07 11:11:50 +0900

    change node modules path

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,5 +3,6 @@ export NVM_DIR="/home/ubuntu/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 echo "$PATH"
 cd /home/ubuntu/yours-truly
+rm -rf /home/ubuntu/yours-truly/node_modules /home/ubuntu/yours-trulypackage-lock.json
 npm install
 npm run build
