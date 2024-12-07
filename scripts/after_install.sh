d356282965101b5f2e8a30a88149914f60e9521a
Brian Lee <brian@atlaslabs.ai>
2024-12-07 12:50:56 +0900

    run npm clean install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,5 @@ export NVM_DIR="/home/ubuntu/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 echo "$PATH"
 cd /home/ubuntu/yours-truly
-sudo rm -rf /home/ubuntu/yours-truly/node_modules/*
-npm install
+npm ci
 npm run build
