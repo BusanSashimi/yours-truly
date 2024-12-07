144e48b926eb6acc570f00253d60326836d06f9e
Brian Lee <brian@atlaslabs.ai>
2024-12-07 10:12:15 +0900

    remove node modules and package lock

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 export NVM_DIR="/home/ubuntu/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-echo "IS this working"
 echo "$PATH"
-echo npm -v
-echo node -v
 cd /home/ubuntu/yours-truly
+rm -rf node_modules package-lock.json
 npm install
 npm run build
