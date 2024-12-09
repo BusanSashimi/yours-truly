168f0d565caba6f2884f084cbf09afc2dbbe0e7c
Brian Lee <brian@atlaslabs.ai>
2024-12-09 18:22:18 +0900

    remove after install

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,6 @@
 export NVM_DIR="/home/ubuntu/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 cd /home/ubuntu/yours-truly
-/home/ubuntu/.nvm/versions/node/v22.12.0/bin/pm2 start npm --name yours-truly -- run start
+npm ci
+npm run build
+pm2 start npm --name yours-truly -- run start
