ccdc8ca46dcc378795982016281090a8ba14ca7c
Brian Lee <brian@atlaslabs.ai>
2024-12-09 18:48:51 +0900

    fix start server script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,7 @@
 export NVM_DIR="/home/ubuntu/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 cd /home/ubuntu/yours-truly
-npm ci
+rm -rf node_modules
+npm install
 npm run build
 pm2 start npm --name yours-truly -- run start
